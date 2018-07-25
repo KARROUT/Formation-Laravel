4ba1519f9eeb4e6ea93d35a8e8f36c82fe0c3820
KARROUT <zoubirkarrout@gmail.com>
2018-07-25 12:34:30 +0100

    fix the problem

---
+++ resources/views/category/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<h1>shox page</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique accusantium magni cupiditate dolorem, iusto aspernatur odio ipsa cumque sed ullam voluptatibus quas velit! Voluptatibus quod ea reiciendis maiores, at deleniti.</p>
+
+@endsection
