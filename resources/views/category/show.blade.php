688b2e230948bd8f2056b180c5ac1442793e40c2
KARROUT <zoubirkarrout@gmail.com>
2018-07-25 12:38:26 +0100

    ajout du lien vers google dans la page show category

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -6,4 +6,6 @@
 <h1>shox page</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique accusantium magni cupiditate dolorem, iusto aspernatur odio ipsa cumque sed ullam voluptatibus quas velit! Voluptatibus quod ea reiciendis maiores, at deleniti.</p>
 
+<a href="http://google.fr">Vers google</a>
+
 @endsection
